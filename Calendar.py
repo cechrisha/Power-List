a9c48ed740d4c447dac1415dc1e1c26a14af6862
cechrisha <119649072+cechrisha@users.noreply.github.com>
2023-08-15 23:50:56 +0000

    making changes to calendar and requirements to link together

--- Calendar.py
+++ Calendar.py
@@ -1,5 +1,7 @@
-#virtualenv env
-#.\env\Scripts\activate.bat
-#pip install -r requirements.txt
+#python code to demonstrate the working of the calendar function to 
+#print calendar
 
+#important calendar module for calendar operation
+
+pip install -r requirements.txt
 
